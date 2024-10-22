9088d8a6cceae33379edb9b0f0a74e5aa5ebc76d
BrunoMagno <bmagnoserver@gmail.com>
2024-10-22 09:21:23 -0300

    Update Project

---
+++ api/icons.js
@@ -0,0 +1,17 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const icons = JSON.parse(readFileSync(path.resolve('./dist/icons.json')));
+  const queryIcons = req.query.i ? req.query.i.split(',') : [];
+  const theme = req.query.theme || 'light';
+
+  const result = {};
+  for (const icon of queryIcons) {
+    if (icons[icon]) {
+      result[icon] = icons[icon];  // Pega o conteúdo SVG
+    }
+  }
+
+  res.json(result);  // Retorna os ícones solicitados
+}
